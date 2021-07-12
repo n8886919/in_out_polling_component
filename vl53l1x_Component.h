6aebf49ceb9685c0cbc38553e5cb65126c8d662f
NolanWang <nolanasd123@gmail.com>
2021-07-13 00:48:53 +0800

    component add stopwatch

--- vl53l1x_Component.h
+++ vl53l1x_Component.h
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <VL53L1X.h>
+#include <StopWatch.h>
 #include "esphome.h"
 
 class MyCustomSensor : public Component {
@@ -8,11 +9,19 @@ class MyCustomSensor : public Component {
   VL53L1X VL53L1X_1, VL53L1X_2;
   uint16_t last_mm1 = 0;
   uint16_t last_mm2 = 0;
+  uint16_t mm1, mm2, change;
+  uint32_t elapsed_1, elapsed_2;
   Sensor *tof_1 = new Sensor();
   Sensor *tof_2 = new Sensor();
 
+  StopWatch SWarray[2];
+  Sensor *timer_1 = new Sensor();
+  Sensor *timer_2 = new Sensor();
+
   void setup() override {
     // This will be called by App.setup()
+    SWarray[0].start();
+    SWarray[1].start();
     Wire.begin();
     Wire.setClock(400000); // use 400 kHz I2C
 
@@ -39,14 +48,24 @@ class MyCustomSensor : public Component {
   }
 
   void loop() override {
-
-    uint16_t mm1 = VL53L1X_1.read();
+    mm1 = VL53L1X_1.read();
     if (!VL53L1X_1.timeoutOccurred()) {
-      uint16_t change = abs(last_mm1 - mm1);
+      change = abs(last_mm1 - mm1);
       if (change > 100) {
-        ESP_LOGI("VL53L1X 1 custom sensor", " address: %x, change: % u",  VL53L1X_1.getAddress(), change);
+        SWarray[0].stop();
+
         tof_1->publish_state(mm1);
+        elapsed_1 = SWarray[0].elapsed();
+
+        if (elapsed_1 < 5000) {
+          timer_1->publish_state(elapsed_1);
+        }
         last_mm1 = mm1;
+        SWarray[0].reset();
+        SWarray[0].start();
+        ESP_LOGI("VL53L1X 1 custom sensor", "address: %x,\tchange: % u,\tdt: %u",
+                 VL53L1X_1.getAddress(), change, elapsed_1);
+
       } else {
         ESP_LOGD("VL53L1X 1 custom sensor", "change  %u mm", change);
       }
@@ -54,13 +73,22 @@ class MyCustomSensor : public Component {
       ESP_LOGE("VL53L1X 1 custom sensor", "Timeout during read().");
     }
 
-    uint16_t mm2 = VL53L1X_2.read();
+    mm2 = VL53L1X_2.read();
     if (!VL53L1X_2.timeoutOccurred()) {
-      uint16_t change = abs(last_mm2 - mm2);
+      change = abs(last_mm2 - mm2);
       if (change > 100) {
-        ESP_LOGI("VL53L1X 2 custom sensor", " address: %x, change: % u",  VL53L1X_2.getAddress(), change);
+        SWarray[1].stop();
+
         tof_2->publish_state(mm2);
+        elapsed_2 = SWarray[1].elapsed();
+        if (elapsed_2 < 5000) {
+          timer_2->publish_state(elapsed_2);
+        }
         last_mm2 = mm2;
+        SWarray[1].reset();
+        SWarray[1].start();
+        ESP_LOGW("VL53L1X 2 custom sensor", "address: %x,\tchange: % u,\tdt: %u",
+                 VL53L1X_2.getAddress(), change, elapsed_2);
       } else {
         ESP_LOGD("VL53L1X 2 custom sensor", "change  %u mm", change);
       }
